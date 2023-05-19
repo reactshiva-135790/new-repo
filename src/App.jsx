6fe1aa045d31573a9dde52168fe72abc7db950c5
reactshiva-135790 <shivamaurya838@gmail.com>
2023-05-19 12:48:37 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import { useSelector, useDispatch } from 'react-redux'
-
-import { increment, decrement,incrementBy } from "./redux/feature/couterSlice"
-
 function App() {
-  const counter = useSelector(state => state.counter.counter);
-  const dispatch = useDispatch();
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <div className="card">
-        <h1>{counter}</h1>
-        <button onClick={() => dispatch(increment())}>
-          Add
-        </button>
-        <button onClick={() => dispatch(decrement())}>
-          Remove
-        </button>
-        <button onClick={() => dispatch(incrementBy(10))}>
-          Additionally + 10 more
-        </button>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-
-import counterReducer  from "./feature/couterSlice";
-
-export default configureStore({
-    reducer : {
-      counter : counterReducer,
-    }
-})
